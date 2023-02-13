f651641d489f0c23f1f4d5aab65e5a8f286e586d
Adam Measel <ameasel55@gmail.com>
2023-02-13 14:51:47 -0600

    initial commit

---
+++ Classes/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Classes
+{
+	public class Car
+	{
+	
+		public Car() //default constructor
+		{
+			
+		}
+
+		public Car(string make, string model, int year) // custom constructor
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+		}
+
+
+
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+	}
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // dot.notation
+            Car myCar = new Car();
+            myCar.Make = "Chevrolet";
+            myCar.Model = "Geo Metro";
+            myCar.Year = 1989;
+
+            Console.WriteLine($"Make of the car: {myCar.Make}");
+            Console.WriteLine($"Model of the car: {myCar.Model}");
+            Console.WriteLine($"Year of the car: {myCar.Year}");
+
+            // object initializer
+            var mazda = new Car()
+            {
+                Make = "Ford",
+                Model = "Pinto",
+                Year = 1973
+            };
+            Console.WriteLine();
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine();
+
+            Console.WriteLine($"Make of the car: {mazda.Make}");
+            Console.WriteLine($"Model of the car: {mazda.Model}");
+            Console.WriteLine($"Year of the car: {mazda.Year}");
+
+            // custom
+            var pontiac = new Car("Pontiac", "Aztek", 2001);
+
+            Console.WriteLine();
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine();
+
+            Console.WriteLine($"Make of the car: {pontiac.Make}");
+            Console.WriteLine($"Model of the car: {pontiac.Model}");
+            Console.WriteLine($"Year of the car: {pontiac.Year}");
+
+            var carList = new List<Car>();
         }
     }
 }
